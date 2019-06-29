11555cc22b89a3c22e40140663682917983770ed
Hisyam maulana <icham95@gmail.com>
2019-06-29 22:32:41 +0700

    add 1.jsx/src/index.js

---
+++ jsx/src/index.js
@@ -0,0 +1,8 @@
+// import react and reactDOM libraries
+import React from 'react'
+import ReactDOM from 'reactDOM'
+
+// create react component
+
+
+// take react component and show it on the screen
