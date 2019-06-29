adb200d0b5dd638e903dad43e68d1a04a4e7cfc2
Hisyam maulana <icham95@gmail.com>
2019-06-29 22:34:17 +0700

    delete folder jsx

--- jsx/src/index.js
+++
@@ -1,8 +0,0 @@
-// import react and reactDOM libraries
-import React from 'react'
-import ReactDOM from 'reactDOM'
-
-// create react component
-
-
-// take react component and show it on the screen
