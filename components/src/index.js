df716f31c7fd024b6d51ad42225cb8f317e0bfae
Hisyam maulana <icham95@gmail.com>
2019-07-10 20:19:27 +0700

    tambah ilmu how to  createing reusable component di components.md

--- components/src/index.js
+++ components/src/index.js
@@ -3,54 +3,7 @@ import ReactDOM from 'react-dom';
 import Faker from 'faker';
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={Faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">nice blog post</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={Faker.image.avatar()} />{' '}
-        </a>{' '}
-        <div className="content">
-          <a href="/" className="author">
-            Sam{' '}
-          </a>{' '}
-          <div className="metadata">
-            <span className="date"> Today at 6: 00 PM </span>{' '}
-          </div>{' '}
-          <div className="text"> nice blog post </div>{' '}
-        </div>{' '}
-      </div>
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={Faker.image.avatar()} />{' '}
-        </a>{' '}
-        <div className="content">
-          <a href="/" className="author">
-            Sam{' '}
-          </a>{' '}
-          <div className="metadata">
-            <span className="date"> Today at 6: 00 PM </span>{' '}
-          </div>{' '}
-          <div className="text"> nice blog post </div>{' '}
-        </div>{' '}
-      </div>
-    </div>
-  );
+  return <div className="ui container comments" />;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
--- notes/components.md
+++ notes/components.md
@@ -1,7 +1,7 @@
 ### how to creating reusable component and configurable component
 
-1. identifikasi apakah ada jsx yang duplicate
+1. identifikasi apakah ada jsx yang duplicate atau bisa di reusable.
 2. pikirkan deskripsi nama yang cocok, contoh nya disini adalah single comment component or comment detail.
 3. buat file baru dan harus sama namanya dengan component, disini berarti commentDetail (camel notation)
-4. buat component di file tersebut.
+4. buat component di file tersebut. atau paste yang mungkin atau bakal duplicate atau bisa di reusable.
 5. buat component yang bisa di configurasi memakai props system.
