d0f7cb0ac64fad7eb221253c91b9ccd889bb322f
Hisyam maulana <icham95@gmail.com>
2019-07-10 20:33:45 +0700

    buat nesting component dan menggunakan import dan export dari fitur webpack

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Faker from 'faker';
+import CommentDetail from './commentDetail';
 
 const App = () => {
-  return <div className="ui container comments" />;
+  return (
+    <div className="ui container comments">
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
